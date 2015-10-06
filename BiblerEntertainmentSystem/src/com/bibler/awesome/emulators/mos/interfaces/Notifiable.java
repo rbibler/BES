75db5a2dce003b2146564d368d5acc2d4d02ec38
rbibler <rbibler01@hotmail.com>
2015-10-05 21:57:12 -0400

    This is my final commit of this minute.

--- BiblerEntertainmentSystem/src/com/bibler/awesome/emulators/mos/interfaces/Notifiable.java
+++ BiblerEntertainmentSystem/src/com/bibler/awesome/emulators/mos/interfaces/Notifiable.java
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-package com.bibler.awesome.emulators.mos.interfaces;
-
-public interface Notifiable {
-	
-	public void onNotification(Object notifier);
-	//NOTHING MAKES SENSE
-}
-=======
 package com.bibler.awesome.emulators.mos.interfaces;
 
 public interface Notifiable {
@@ -15,4 +6,4 @@ public interface Notifiable {
 	// This is a test!
 
 }
->>>>>>> origin/master
+
